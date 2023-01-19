6a9fffcbea189e10fa53b34a4477e68eefc206bb
Aishik Bera <Aishik.Bera@clinisys.com>
2023-01-19 10:08:40 +0530

    DemoEnum1

---
+++ Application/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Application
+{
+    enum WeekDays
+    {
+        Monday=1,
+        Tuesday,Wednesday,Thursday,Friday,Saturday,
+        Sunday=0
+    };
+    class Program
+    {
+      static void Main(string[] args)
+      {
+        for(int i=0;i<7;i++)
+        {
+            System.Console.WriteLine("Day "+ (i+1) +" of Week = "+(WeekDays)i);
+        }
+      }
+        
+    }
+}
